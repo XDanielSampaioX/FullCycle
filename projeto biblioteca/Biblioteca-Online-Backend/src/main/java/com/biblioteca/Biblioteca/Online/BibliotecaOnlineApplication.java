8c8e49bb7334b187f67225602b76010c499277d9
Daniel Sampaio <danielsampaio127@gmail.com>
2026-09-14 00:39:17 -0300

    feat: criando projetos da biblioteca online

---
+++ projeto biblioteca/Biblioteca-Online-Backend/src/main/java/com/biblioteca/Biblioteca/Online/BibliotecaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.Biblioteca.Online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaOnlineApplication.class, args);
+	}
+
+}
